9d13e43511bc878c43ceaeb5a7aaf69e112cddbb
Mehraj Hosen <meherajhosen786@gmail.com>
2025-02-24 01:12:51 +0600

    add components

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Logo from '../Logo'
 import { NavLink } from 'react-router-dom'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function Header() {
   return (
-    <nav className='bg-[#1C1C1C] text-white pt-[24px] pb-[24px]'>
+    <>
+    <nav className='bg-[#1C1C1C] text-white py-[24px] '>
       <div className='container flex justify-between items-center'>
         <Logo/>
         <div className="navlist mx-auto">
@@ -18,10 +19,15 @@ function Header() {
           </ul>
         </div>
         <div className="right">
+          <button>Let's Talk <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffff" class="bi bi-arrow-up-right" 
+          viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"/>
+</svg></button>
           
         </div>
       </div>
     </nav>
+    </>
   )
 }
 
