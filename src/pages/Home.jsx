7d5f604a22dff2cae62f60b36bcbb85406575558
Mehraj Hosen <meherajhosen786@gmail.com>
2025-02-24 23:03:12 +0600

    add service

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -112,7 +112,39 @@ laudantium, totam rem aperiamc</p>
       </div>
       {/* about end  */}
       {/* service start  */}
-      <div id="service"></div>
+      <div id="services pt-[109px] pb-[139px]">
+        <div className="container">
+          <div className="serviceTitle text-center">
+            <p>Popular Services</p>
+            <h2 className="w-[600px] mx-auto font-bold text-[42px] text-center">My <span className="text-[#C9F31D]">Special Service</span> For your Business Development</h2>
+          </div>
+          <div className="grid grid-cols-12">
+            <div className="col-span-6 ">
+<div className="box flex items-center justify-between w-[576px] h-[140px] bg-[#1F1F1F] rounded-[20px] px-[20px] py-[35px]">
+<div className="text">
+<h3 className="text-[32px] text-bold text-white">Responsive Web Design</h3>
+<p className="text-[16px]">Responsive web design for any device</p>
+</div>
+<div className="icon p-[12px] bg-[#C9F31D] rounded-full">
+  <span className="">
+  <svg  xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 16 16"><path  fill="currentColor" d="M8.5.75a.75.75 0 0 0-1.5 0v5.19L4.391 3.33a.75.75 0 1 0-1.06 1.061L5.939 7H.75a.75.75 0 0 0 0 1.5h5.19l-2.61 2.609a.75.75 0 1 0 1.061 1.06L7 9.561v5.189a.75.75 0 0 0 1.5 0V9.56l2.609 2.61a.75.75 0 1 0 1.06-1.061L9.561 8.5h5.189a.75.75 0 0 0 0-1.5H9.56l2.61-2.609a.75.75 0 0 0-1.061-1.06L8.5 5.939z"/></svg>
+  </span>
+</div>
+</div>
+            </div>
+            <div className="col-span-6 ml-auto">
+world
+            </div>
+            <div className="col-span-6">
+hello
+            </div>
+            <div className="col-span-6 ml-auto">
+world
+            </div>
+
+          </div>
+        </div>
+      </div>
       {/* service end  */}
       {/* skill start  */}
       <div id="skill"></div>
