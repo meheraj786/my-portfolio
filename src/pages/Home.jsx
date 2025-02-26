92e926b3f3bcf62a0ef9d5bf226de93213d85f57
Mehraj Hosen <meherajhosen786@gmail.com>
2025-02-26 23:13:43 +0600

    fix bugs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -913,10 +913,90 @@ function Home() {
         <div className="container">
           <div className="grid grid-cols-12">
             <div className="col-span-4">
-              hello
+              <p>Get in Touch</p>
+              <h3>Let’s Talk For your
+              Next Projects</h3>
+              <p>Sed ut perspiciatis unde omnis iste natus to
+voluptatem accusantium doloremque
+laudantium, totam rem aperiamc</p>
+<ul>
+  <li className="flex items-center">                  
+    <span className="mr-[13px] p-[12px] bg-[#C9F31D] rounded-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="25"
+                      height="25"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M8.5.75a.75.75 0 0 0-1.5 0v5.19L4.391 3.33a.75.75 0 1 0-1.06 1.061L5.939 7H.75a.75.75 0 0 0 0 1.5h5.19l-2.61 2.609a.75.75 0 1 0 1.061 1.06L7 9.561v5.189a.75.75 0 0 0 1.5 0V9.56l2.609 2.61a.75.75 0 1 0 1.06-1.061L9.561 8.5h5.189a.75.75 0 0 0 0-1.5H9.56l2.61-2.609a.75.75 0 0 0-1.061-1.06L8.5 5.939z"
+                      />
+                    </svg>
+                  </span>
+                  2+ Years Experience</li>
+                  <li className="flex items-center">                  
+    <span className="mr-[13px] p-[12px] bg-[#C9F31D] rounded-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="25"
+                      height="25"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M8.5.75a.75.75 0 0 0-1.5 0v5.19L4.391 3.33a.75.75 0 1 0-1.06 1.061L5.939 7H.75a.75.75 0 0 0 0 1.5h5.19l-2.61 2.609a.75.75 0 1 0 1.061 1.06L7 9.561v5.189a.75.75 0 0 0 1.5 0V9.56l2.609 2.61a.75.75 0 1 0 1.06-1.061L9.561 8.5h5.189a.75.75 0 0 0 0-1.5H9.56l2.61-2.609a.75.75 0 0 0-1.061-1.06L8.5 5.939z"
+                      />
+                    </svg>
+                  </span>
+                  2+ Years Experience</li>
+                  <li className="flex items-center">                  
+    <span className="mr-[13px] p-[12px] bg-[#C9F31D] rounded-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="25"
+                      height="25"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M8.5.75a.75.75 0 0 0-1.5 0v5.19L4.391 3.33a.75.75 0 1 0-1.06 1.061L5.939 7H.75a.75.75 0 0 0 0 1.5h5.19l-2.61 2.609a.75.75 0 1 0 1.061 1.06L7 9.561v5.189a.75.75 0 0 0 1.5 0V9.56l2.609 2.61a.75.75 0 1 0 1.06-1.061L9.561 8.5h5.189a.75.75 0 0 0 0-1.5H9.56l2.61-2.609a.75.75 0 0 0-1.061-1.06L8.5 5.939z"
+                      />
+                    </svg>
+                  </span>
+                  2+ Years Experience</li>
+                  <li className="flex items-center">                  
+    <span className="mr-[13px] p-[12px] bg-[#C9F31D] rounded-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="25"
+                      height="25"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M8.5.75a.75.75 0 0 0-1.5 0v5.19L4.391 3.33a.75.75 0 1 0-1.06 1.061L5.939 7H.75a.75.75 0 0 0 0 1.5h5.19l-2.61 2.609a.75.75 0 1 0 1.061 1.06L7 9.561v5.189a.75.75 0 0 0 1.5 0V9.56l2.609 2.61a.75.75 0 1 0 1.06-1.061L9.561 8.5h5.189a.75.75 0 0 0 0-1.5H9.56l2.61-2.609a.75.75 0 0 0-1.061-1.06L8.5 5.939z"
+                      />
+                    </svg>
+                  </span>
+                  2+ Years Experience</li>
+</ul>
             </div>
             <div className="col-span-7 ml-auto">
-              world
+              <div className="grid grid-cols-12">
+                <div className="col-span-6  ml-auto">
+                  hello 
+                </div>
+                <div className="col-span-6  ml-auto">
+                world
+                </div>
+                <div className="col-span-12  ml-auto">
+                  universe
+                </div>
+                
+
+              </div>
+              <button className=" ml-auto">Send Message</button>
             </div>
           </div>
         </div>
