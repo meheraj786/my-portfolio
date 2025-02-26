fd0e8fee901f357f5a2729ede5b36281d692098f
Mehraj Hosen <meherajhosen786@gmail.com>
2025-02-26 16:19:33 +0600

    footer start

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Logo from '../Logo.jsx'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+    <div className="footer pt-[79px]">
+      <div className="container py-[114px]  bg-black rounded-[20px]">
+        <div className="grid grid-cols-12">
+          <div className="col-span-4 mx-auto">
+            <Logo/>
+          </div>
+          <div className="col-span-4 mx-auto">
+            wolrd
+          </div>
+          <div className="col-span-4 mx-auto">
+            go
+          </div>
+        </div>
+
+      </div>
+    </div>
+    </>
   )
 }
 
