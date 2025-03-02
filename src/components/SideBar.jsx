8945219d4367560e3f95990cb0629669d8b8ac28
Mehraj Hosen <meherajhosen786@gmail.com>
2025-03-03 00:22:58 +0600

    bug fix

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  Drawer,
+  Button,
+  Typography,
+  IconButton,
+  ThemeProvider, // Import ThemeProvider
+} from "@material-tailwind/react";
+
+export default function SideBar() {
+
+
+  return (
+    
+  );
+}
--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons'; // Ensure this import
 
+
 function Header() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
